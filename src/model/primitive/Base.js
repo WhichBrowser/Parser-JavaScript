cc821219d2ea9d265a90cb3b1c0c5dcfb12bd9b0
Simone Mariotti <simone.mariotti@piksel.com>
2017-06-27 17:30:36 +0200

    Added missing JSDoc

--- src/model/primitive/Base.js
+++ src/model/primitive/Base.js
@@ -1,8 +1,23 @@
+/**
+ * @internal
+ */
 class Base {
+  /**
+   * Set the properties of the object the the values specified in the object
+   *
+   * @param  {Object|null} defaults  An Object, the key of an element determines the name of the property
+   */
   constructor(defaults) {
     defaults && this.set(defaults);
   }
 
+  /**
+   * Set the properties of the object the the values specified in the object
+   *
+   * @param  {Object}  properties  An Object, the key of an element determines the name of the property
+   *
+   * @internal
+   */
   set(properties) {
     Object.assign(this, properties);
   }
