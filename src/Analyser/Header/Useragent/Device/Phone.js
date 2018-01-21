9bb05b9f2588557c9cc05fbcbb8f476bb610a4d2
Simone Mariotti <simone@mariotti.me>
2018-01-21 18:24:55 +0100

    Update Phone

--- src/Analyser/Header/Useragent/Device/Phone.js
+++ src/Analyser/Header/Useragent/Device/Phone.js
@@ -5,13 +5,26 @@ const Constants = require('../../../../constants');
 class Phone {
   static detectPhone(ua) {
     Phone.detectNttTeless.call(this, ua);
+    Phone.detectSnom.call(this, ua);
   }
 
-  /* Casio */
+  /* NTT Teless */
   static detectNttTeless(ua) {
     if (/Product=NTT\/Teless/iu.test(ua)) {
       this.data.device.manufacturer = 'NTT';
       this.data.device.model = 'Teless';
+      this.data.device.identified |= Constants.id.MATCH_UA;
+      this.data.device.type = Constants.deviceType.MOBILE;
+      this.data.device.subtype = Constants.deviceSubType.DESKTOP;
+    }
+  }
+
+  /* SNOM */
+  static detectSnom(ua) {
+    let match;
+    if ((match = /snom(.+)-SIP/iu.exec(ua))) {
+      this.data.device.manufacturer = 'SNOM';
+      this.data.device.model = match[1] + ' IP Telephone';
       this.data.device.identified |= Constants.id.MATCH_UA;
       this.data.device.type = Constants.deviceType.MOBILE;
       this.data.device.subtype = Constants.deviceSubType.DESKTOP;
